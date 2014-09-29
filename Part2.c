f8db10e721eac563779e236c29d6b57704eebea2
Pedro Santiago <futurewatertao@hotmail.com>
2014-09-29 19:25:55 +0100

    Diamond Scrublord said he helped.

--- Part2.c
+++ Part2.c
@@ -14,6 +14,11 @@
 #define FALSE 0
 #define TRUE 1
 
+#define F 0x7E
+#define A 0x03
+#define C 0x03
+#define K 0x00
+
 volatile int STOP = FALSE;
 
 int main(int argc, char** argv) {
@@ -39,8 +44,7 @@ int main(int argc, char** argv) {
     newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
+    
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
@@ -52,38 +56,22 @@ int main(int argc, char** argv) {
       perror("tcsetattr");
       exit(-1);
     }
-	
-    char inputstr[256];
     
-    printf("Enter string to transmit: ");
+    unsigned char SET[5];
     
-    gets(inputstr);
+    SET[0] = F;
+    SET[1] = A;
+    SET[2] = C;
+    SET[3] = SET[1] ^ SET[2];
+    SET[4] = F;
 
-    res = write(fd,inputstr,strlen(inputstr)+1);
+    res = write(fd,SET,sizeof(SET));
        
     printf("Written %d bytes.\n", res);
-    
-    /*if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }*/
-    
-    printf("Receive mode set!\n");
-
-    while (STOP==FALSE) {       /* loop for input */
-      res = read(fd,buf,1);   /* returns after 5 chars have been input */
-      buf[res] = 0;               /* so we can printf... */
-      printf("%s", buf, res);
-      if (buf[0] == '\0') 
-	STOP=TRUE;
-    }
-
-    printf("\n");
-
-    sleep(3);
     
     tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
 
     return 0;
 }
+
