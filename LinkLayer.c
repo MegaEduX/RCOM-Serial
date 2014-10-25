451cc567ef5bfa482d13b8a8eb9848fd0063eb50
Eduardo Almeida <megaedux@me.com>
2014-10-25 19:01:10 +0100

    EH.

--- LinkLayer.c
+++ LinkLayer.c
@@ -11,12 +11,14 @@
 void llsetup(char *port, int baudRate, unsigned int sequenceNumber, unsigned int timeout, unsigned int maxRetries) {
 	linkLayerInstance = (LinkLayer *) malloc(sizeof(LinkLayer));
 	
-	strcpy(linkLayerInstance->port, port);
+	//malloc()
+	
+	/*strcpy(linkLayerInstance->port, port);
 	
 	linkLayerInstance->baudRate = baudRate;
 	linkLayerInstance->sequenceNumber = sequenceNumber;
 	linkLayerInstance->timeout = timeout;
-	linkLayerInstance->maxRetries = maxRetries;
+	linkLayerInstance->maxRetries = maxRetries;*/
 }
 
 void lltoggle() {
